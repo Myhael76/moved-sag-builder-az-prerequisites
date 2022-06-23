d4da9ce9a4ad8aae9ee649b84a599e0a7a502d4a
Myhael76 <48013126+Myhael76@users.noreply.github.com>
2022-06-23 20:34:03 +0200

    Repo moved to Software AG

--- azureCloudResources/bash/00.generateLocalSshKeys.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-. ./setEnv.sh
-
-echo "Creating the local ssh keys..."
-
-ssh-keygen -m PEM -t rsa -b 4096
-
-echo "Created the local ssh keys"
--- empowerSdc/Example.setSUIFEmpowerCredentials.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-export WMT_EMPOWER_USER="myUserName"
-export WMT_EMPOWER_USER="myPassword"
